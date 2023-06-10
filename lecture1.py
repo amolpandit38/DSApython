a0d51e4c58c85e73bae10a30d18ef6838b4ad089
amolpandit38 <amol38.ap@gmail.com>
2023-06-10 21:22:58 +0530

    adding lecture 1 and 2

---
+++ lecture1.py
@@ -0,0 +1,34 @@
+"""
+1. Interpreted Language
+2. Object-Oriented Language
+3. Dynamically typed Language
+4. Ease of writing code
+5. Scope of Python
+"""
+
+print("Hello World")
+
+# variables
+# rules - can start with lowercase uppercase and underscore
+# it is a element that points to a value in a memory
+
+
+# datatypes
+# integer
+# string
+# float
+
+
+# arithmetic operators
+
+# +, -, /, *, // -> floor division, **,
+
+
+# input
+a = input()
+# input is always a string
+# we can do type conversion
+print(a)
+print(type(a))
+a = int(input())
+type(a)
---
+++ lecture2.py
@@ -0,0 +1,93 @@
+# data structures in python
+
+"""
+1. Linear data structures
+- Arrays
+- Linked List
+- Stack - LIFO, LILO
+- Queue
+- Matrix
+
+2. Non-linear data structures
+- Binary Tree
+- Heap
+- Hash Table
+- Graph
+
+3. Python Specific Data Structures
+- List
+- Tuple
+- Dictionary
+- Set
+
+4. Built-in DS
+5. User-defined DS
+
+"""
+
+# List
+"""
+1. list can have homogeneous data and heterogeneous data
+2. Mutable
+3. Every element in the list has a specific 
+"""
+
+List = [1, 2, "hey", 7, 9]
+print(List)
+
+List = ["you", "can", "do"]
+print(List)
+
+# Nested List
+List = [["hey", "you"], ["do", "it"]]
+print(len(List))
+
+# Negative indexing
+List = [1, 2, 3, "hey", "abcd", 5, 6, 7, 8]
+print(List[-1])
+print(List[-3])
+
+# tuples
+"""
+1. Immutable
+2. can have homogeneous data and heterogeneous data
+3. cannot add and remove in tuple
+"""
+
+Tuple = (1, 2, 4, "hey")
+print(Tuple)
+print(type(Tuple))
+
+Tuple = 1, 2, 4, "hey"
+print(Tuple)
+print(type(Tuple))
+
+print(Tuple[0])
+
+# mapping
+"""
+1. key value pairs {key : value}
+2. can have integer key 
+3. separated by comma
+"""
+
+Dict = {
+    "key": "value"
+}
+print(Dict["key"])
+
+# by using built-in function
+Dict = dict({1: "one", 2: "two"})
+print(Dict)
+
+# Set
+"""
+1. values have to be unique 
+2. We can check for the value is there inside a set and are unordered
+3. can get input with can be same elements during the creation of set
+"""
+
+Set = set([1, 2, 3])
+print(Set)
+
+
